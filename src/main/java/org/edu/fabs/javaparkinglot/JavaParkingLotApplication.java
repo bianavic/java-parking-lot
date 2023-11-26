2418eb60741ead1125fd67810dd4d56151821676
bianavic <bianavic@gmail.com>
2023-11-26 13:48:12 -0300

    initiatlize project

---
+++ src/main/java/org/edu/fabs/javaparkinglot/JavaParkingLotApplication.java
@@ -0,0 +1,13 @@
+package org.edu.fabs.javaparkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaParkingLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaParkingLotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/edu/fabs/javaparkinglot/JavaParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.fabs.javaparkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
