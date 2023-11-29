da673c4360fe94a88aadf970498f3961ad0022f0
bianavic <bianavic@gmail.com>
2023-11-29 14:22:14 -0300

    feat: add SUPER_ADMIN role

--- src/main/java/org/edu/fabs/javaparkinglot/auth/entity/Role.java
+++ src/main/java/org/edu/fabs/javaparkinglot/auth/entity/Role.java
@@ -8,23 +8,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Table(name = "ROLES")
 @Entity
-@Data
-public class Role {
+@EqualsAndHashCode
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private RoleEnum name;
 
